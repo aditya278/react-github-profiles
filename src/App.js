eede68a009114c4073ae5d55331cf3410911adfc
aditya278 <aditya.shukla278@gmail.com>
2020-11-26 18:07:24 +0530

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import './App.css';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+//Importing the components
+import Navbar from './components/Navbar';
+
 export default class App extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Router>
+                <div>
+                    <Navbar title="GitHub Profile Search"/>
+                </div>
+            </Router>
         )
     }
 }
