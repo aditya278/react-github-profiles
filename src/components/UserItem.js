5e76c79507b314bdbde6b1fe760d739668e884a0
aditya278 <aditya.shukla278@gmail.com>
2021-02-08 12:58:26 +0530

    Bug fixes

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 export default function UserItem({login, avatar_url}) {
+    const history = useHistory();
     return (
         <div className="card text-center" style={{maxWidth:300}}>
             <img src={avatar_url} alt="Avatar" className="round-img" style={{width:140}} />
             <h3>{login}</h3>
             <div>
-                <Link to={`react-github-profiles/user/${login}`} className="btn btn-dark btn-sm my1">
+                <div onClick={() => history.replace(`/react-github-profiles/user/${login}`)} className="btn btn-dark btn-sm my1">
                     GitHub Profile
-                </Link>
+                </div>
             </div>
         </div>
     )
